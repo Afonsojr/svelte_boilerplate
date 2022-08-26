6933f2a4d032d4270bb0757195e3c43367468154
Afonsojr <lafonsojr@gmail.com>
2022-08-26 15:05:40 -0300

    Commit inicial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-800 place-items-center">
+	<div class="content-center">
+		<div class="relative sm:pt-6 sm:pb-24 mt-6">
+			<main class="mt-16">
+				<div class="">
+					<div class="lg:grid lg:grid-cols-12 lg:gap-8">
+						<div
+							class="sm:text-left md:max-w-2xl lg:col-span-6 lg:text-left lg:flex lg:items-center"
+						>
+							<div>
+								<h1
+									class="text-4xl sm:text-5xl tracking-normal font-extrabold text-white sm:leading-none lg:text-6xl"
+								>
+									The complete developer tool for <span class="text-indigo-400">
+										Tailwind CSS</span
+									>, <span class="text-fuchsia-600"> Sass </span> and
+									<span class="svelte_color">Svelte</span>
+								</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.svelte_color {
+		color: #ff3e00;
+	}
+</style>
